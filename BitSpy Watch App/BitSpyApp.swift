65399b8146b06d908252397c1565a4d211410143
martinkarlik <kar@captureone.com>
2023-03-08 13:16:00 +0100

    Initial Commit

---
+++ BitSpy Watch App/BitSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitSpyApp.swift
+//  BitSpy Watch App
+//
+//  Created by Martin Karlik on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BitSpy_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitSpy/BitSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitSpyApp.swift
+//  BitSpy
+//
+//  Created by Martin Karlik on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BitSpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
