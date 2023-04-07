1f5e601061b9b2005f205d596b6fb912263f8c7a
martinkarlik <kar@captureone.com>
2023-04-07 13:57:03 +0200

    Introduce MotionDetectionProvider and detect watch rotation.

---
+++ BeMindfulWatch/WatchAppTypes.swift
@@ -0,0 +1,13 @@
+//
+//  WatchAppTypes.swift
+//  BeMindfulWatch
+//
+//  Created by Martin Karlik on 03/04/2023.
+//
+
+import Foundation
+
+
+protocol MotionDetectionDelegate {
+    func monitorRotationRate()
+}
