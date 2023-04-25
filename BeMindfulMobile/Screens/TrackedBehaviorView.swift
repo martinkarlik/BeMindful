d1294e74ba3156bec80d5ce5486469d7ae015365
Marina Epitropakis <marinaepitropaki@gmail.com>
2023-04-25 11:41:10 +0200

    placeholder works flacky

--- BeMindfulMobile/Screens/TrackedBehaviorView.swift
+++ BeMindfulMobile/Screens/TrackedBehaviorView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 enum TrackedBehavior: String, CaseIterable {
-    case behavior1 = "Behavior 1"
-    case behavior2 = "Behavior 2"
-    case behavior3 = "Behavior 3"
-    case behavior4 = "Behavior 4"
-    case behavior5 = "Behavior 5"
+    case behavior1 = "Nail Picking"
+    case behavior2 = "Nail Biting"
+    case behavior3 = "Cheek Biting"
+    case behavior4 = "Hair Pulling"
+    case behavior5 = "Other..."
 }
 
 struct TrackedBehaviorView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     @AppStorage("tracking") var isTrackedBehaviorViewActive: Bool = true
     @State var selectedBehavior: TrackedBehavior?
+    @State var customBehavior: String = ""
     
     var body: some View {
         VStack {
@@ -35,6 +36,7 @@ struct TrackedBehaviorView: View {
             Spacer()
             
             ForEach(TrackedBehavior.allCases, id: \.self) { behavior in
+                
                 Button(action: {
                     if behavior == selectedBehavior {
                         // Deselect the button if it's already selected
@@ -46,15 +48,29 @@ struct TrackedBehaviorView: View {
                 }) {
                     HStack {
                         if selectedBehavior == behavior {
-                            Label(behavior.rawValue, systemImage: "checkmark")
-                                .foregroundColor(Color.white)
-                                .fontWeight(.regular)
-                                .frame(maxWidth: 350, minHeight: 41)
-                                .foregroundColor(.white)
-                                .background(Color(hex: 0x5E63B6).opacity(0.8))
-                                .cornerRadius(8)
-                                .shadow(color: Color.gray.opacity(1),
-                                        radius: 4, x: 0, y: 2)
+                            if behavior == .behavior5 {
+                                TextField("Enter custom behavior", text: $customBehavior, onCommit: {
+                                    if !customBehavior.isEmpty {
+                                        selectedBehavior = .behavior5
+                                    }
+                                })
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                    .padding(.horizontal, 16)
+                                    .frame(maxWidth: 350, minHeight: 41)
+                                    .background(Color(hex: 0x5E63B6).opacity(0.8))
+                                    .cornerRadius(8)
+                                    .shadow(color: Color.gray.opacity(1), radius: 4, x: 0, y: 2)
+                            } else {
+                                Label(behavior.rawValue, systemImage: "checkmark")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.regular)
+                                    .frame(maxWidth: 350, minHeight: 41)
+                                    .background(Color(hex: 0x5E63B6).opacity(0.8))
+                                    .cornerRadius(8)
+                                    .shadow(color: Color.gray.opacity(1), radius: 4, x: 0, y: 2)
+                                    .labelStyle(TrailingImageLabelStyle())
+                            }
                         }
                         else {
                             Text(behavior.rawValue)
@@ -91,7 +107,7 @@ struct TrackedBehaviorView: View {
                 .padding(.trailing, 16)
                 .shadow(radius: 2, y: 2)
             }
-            .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 16)
+            .padding(.bottom, 16)
             .padding(.trailing, 16)
         }
         .background(Color.white.edgesIgnoringSafeArea(.all))
@@ -99,22 +115,18 @@ struct TrackedBehaviorView: View {
 }
 
 
-
-
-
-
-extension Color {
-    init(hex: Int) {
-        self.init(
-            .sRGB,
-            red: Double((hex >> 16) & 0xff) / 255,
-            green: Double((hex >> 08) & 0xff) / 255,
-            blue: Double((hex >> 00) & 0xff) / 255,
-            opacity: 1
-        )
+struct TrailingImageLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+                .padding(.trailing, 8)
+        }
+        .padding(.vertical, 8)
+        .cornerRadius(8)
+        .padding(.horizontal)
     }
 }
-
 
 struct ChoiceSelection_Previews: PreviewProvider {
     static var previews: some View {
